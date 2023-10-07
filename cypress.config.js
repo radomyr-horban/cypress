5f99692cfab58c9c8398067c1f65f04b78bd0133
Radomyr Horban <horban.radomyr@gmail.com>
2023-10-07 07:29:41 +0300

    Updated localhost in YML file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: 'hn74h7',
   e2e: {
+    // baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
